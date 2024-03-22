b03651d9417131c5e46c2b094f856f7f98250a6c
sean <39540655+carrysocks@users.noreply.github.com>
2024-03-22 16:23:55 +0900

    Title: Carry 피하기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240322/Carry 피하기 2/escaping-carry-2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int n;
+int arr[21];
+
+int main() {
+    cin >> n;
+    for(int i=0;i<n;i++) cin >> arr[i];
+
+    int mx = -1;
+    bool isCarry = false;
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            for(int k=j+1;k<n;k++){
+                
+                isCarry = false;
+                int idx = 1;
+                int d = 1;
+                while(idx++ <= 5){
+                    if(arr[i] % (d*10) / d + 
+                        arr[j] % (d*10) / d + 
+                        arr[k] % (d*10) / d 
+                        > 10
+                    ) isCarry = true;
+                    d *= 10;
+                }
+
+                if(!isCarry) mx = max(mx, arr[i] + arr[j] + arr[k]);
+            }
+        }
+    }
+
+    cout << mx;
+}
