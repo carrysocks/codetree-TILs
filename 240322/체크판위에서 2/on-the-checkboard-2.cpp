0aab7cee897371c9412bc4acda44dd7d0f956ed0
sean <39540655+carrysocks@users.noreply.github.com>
2024-03-22 13:43:21 +0900

    Title: 체크판위에서 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240322/체크판위에서 2/on-the-checkboard-2.cpp
+++ 240322/체크판위에서 2/on-the-checkboard-2.cpp
@@ -17,18 +17,18 @@ int main() {
     }
 
     int ans = 0;
-    int color1 = board[0][0];
     for(int i=1;i<n-1;i++){
         for(int j=1;j<m-1;j++){
-            if(board[i][j] == color1) continue;
             for(int k=i+1;k<n-1;k++){
                 for(int l=j+1;l<m-1;l++){
-                    if(board[k][l] == color1) ans++;
+                    if(board[0][0] != board[i][j] && 
+                        board[i][j] != board[k][l] &&
+                        board[k][l] != board[n-1][m-1]
+                    ) ans++;
                 }
             }
         }
     }
 
-    if(board[0][0] == board[n-1][m-1]) cout << 0;
-    else cout << ans;
+    cout << ans;
 }
